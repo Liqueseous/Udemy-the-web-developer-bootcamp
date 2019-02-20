0f188b20d9de9da9362bec08946306dd82054c55
Ryan DeLosh <ryguy1201@gmail.com>
2019-02-19 19:49:21 -0500

    Database Start
    start of learning about databases (MongoDB)

--- yelpCamp/app.js
+++ yelpCamp/app.js
@@ -5,24 +5,25 @@ var myport = 3000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
+var campgrounds = [
+  {name: "Salmon Creek", image: "https://farm3.staticflickr.com/2116/2164766085_0229ac3f08.jpg"},
+  {name: "Mountain Heights", image: "https://farm8.staticflickr.com/7268/7121859753_e7f787dc42.jpg"},
+  {name: "Granite Hill", image: "https://farm4.staticflickr.com/3911/14707566622_af236f9b65.jpg"}
+]
+
 app.get('/', (req, res) => {
   res.render('landing');
 });
 
 app.get('/campgrounds', (req, res) => {
-  var campgrounds = [
-    {name: "Salmon Creek", image: "https://farm3.staticflickr.com/2116/2164766085_0229ac3f08.jpg"},
-    {name: "Mountain Heights", image: "https://farm8.staticflickr.com/7268/7121859753_e7f787dc42.jpg"},
-    {name: "Granite Hill", image: "https://farm4.staticflickr.com/3911/14707566622_af236f9b65.jpg"}
-  ]
-
   res.render('campgrounds', {campgrounds: campgrounds});
 });
 
 app.post('/campgrounds', (req, res) => {
-  res.send("You posted to campgrounds");
   // get data from form and add to campgrounds array
+  campgrounds.push({name: req.body.name, image: req.body.image});
   // redirect back to campgrounds page
+  res.redirect('/campgrounds');
 });
 
 app.get('/campgrounds/new', (req, res) => {
